c1d28708689bc0dca5d874632e5fce361197e3d3
wuyou1102 <jotey@qq.com>
2019-02-26 11:06:31 +0800

    add

--- libs/Config/Cases.py
+++ libs/Config/Cases.py
@@ -3,6 +3,7 @@ from libs.UserInterface.TestPages.EthernetTest import Ethernet
 from libs.UserInterface.TestPages.SwitchTest import Switch
 from libs.UserInterface.TestPages.USBTest import USB
 from libs.UserInterface.TestPages.LightTest import Light
+from libs.UserInterface.TestPages.LEDTest import LED
 from libs.UserInterface.TestPages.FPVTest import FPV
 from libs.UserInterface.TestPages.RF_ReceiveTest import \
     Receive2410, Receive2450, Receive2475, \
@@ -11,7 +12,7 @@ from libs.UserInterface.TestPages.RF_TransmitTest import \
     Transmit2410, Transmit2450, Transmit2475, \
     Transmit5750, Transmit5800, Transmit5850
 
-PCBA_CASES = [Ethernet, Switch, Light, USB]
+PCBA_CASES = [Ethernet, Switch, Light, LED, USB]
 MACHINE_CASES = [FPV]
 RF_CASES = [
     Receive2410, Receive2450, Receive2475, Receive5750, Receive5800, Receive5850,
---
+++ libs/UserInterface/TestPages/LEDTest.py
@@ -0,0 +1,47 @@
+# -*- encoding:UTF-8 -*-
+import wx
+import logging
+import Base
+from libs.Config import Font
+from libs.Config import Color
+from libs.Config import String
+
+logger = logging.getLogger(__name__)
+
+
+class LED(Base.TestPage):
+    def __init__(self, parent, type):
+        Base.TestPage.__init__(self, parent=parent, type=type)
+        self.count = 0
+
+    def init_test_sizer(self):
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        output = wx.TextCtrl(self, -1, "", style=wx.TE_MULTILINE | wx.TE_READONLY)
+        output.AppendText(u"请检查主板上的LED灯是否全亮\n")
+        output.AppendText(u"\n")
+        output.AppendText(u"判断条件:\n")
+        output.AppendText(u"  LED灯全亮  PASS\n")
+        output.AppendText(u"  其他情况    FAIL\n")
+        output.SetInsertionPointEnd()
+        output.SetBackgroundColour(Color.LightSkyBlue1)
+        output.SetFont(Font.COMMON_1_LARGE_BOLD)
+        sizer.Add(output, 1, wx.EXPAND | wx.ALL, 1)
+        return sizer
+
+    def before_test(self):
+        pass
+
+    def start_test(self):
+        self.FormatPrint(info="Started")
+
+    def stop_test(self):
+        self.FormatPrint(info="Stop")
+
+    @staticmethod
+    def GetName():
+        return u"LED灯测试"
+
+    @staticmethod
+    def GetFlag(t):
+        if t == "PCBA":
+            return String.PCBA_LED
